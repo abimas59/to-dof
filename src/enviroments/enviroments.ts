275fb112dac2462e548b049a45f16d3763631452
Abimael Brito <160194472+abimas59@users.noreply.github.com>
2024-11-14 20:44:35 -0300

    Update enviroments.ts

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -1,5 +1,5 @@
 export const enviroment = {
     production: false,
-    baseUrl: "http://localhost:8080/todos",
-    baseUrlOpen:"http://localhost:8080/todos/open"
+    baseUrl: "https://todo-abima-7d8876e0d5f4.herokuapp.com/todos",
+    baseUrlOpen:"https://todo-abima-7d8876e0d5f4.herokuapp.com/todos/open"
 }
